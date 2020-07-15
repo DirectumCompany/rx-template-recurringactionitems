91daaff254cf0bed6f2cff6108fbef2ad2f6cf39
Lebedev_VA <Lebedev_VA@directum.ru>
2020-07-15 09:15:15 +0400

    Восстановление удаленных файлов

---
+++ DirRX.PeriodicActionItemsTemplate/DirRX.PeriodicActionItemsTemplate.Server/ModuleServerFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Domain;
+
+namespace DirRX.PeriodicActionItemsTemplate.Server
+{
+  public class ModuleFunctions
+  { 
+  }
+}
---
+++ DirRX.Solution/DirRX.Solution.ClientBase/ModuleClientFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using System.IO;
+
+namespace DirRX.Solution.Client
+{
+  public class ModuleFunctions
+  {
+  }
+}
