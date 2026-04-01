387ba3fa972c49abdc44a09e7e424f2948d9c990
DirectumRXUser <directumrxuser@c.ru>
2026-04-02 03:47:15 +0400

    Удаление зависимости от GD (возврат к состоянию до 19.03.2026) от коммита "Фильтрация черновиков при отправке повторяющихся поручений"

--- DirRX.PeriodicActionItemsTemplate/DirRX.PeriodicActionItemsTemplate.Server/RepeatSetting/RepeatSettingServerFunctions.cs
+++ DirRX.PeriodicActionItemsTemplate/DirRX.PeriodicActionItemsTemplate.Server/RepeatSetting/RepeatSettingServerFunctions.cs
@@ -284,13 +284,6 @@ namespace DirRX.PeriodicActionItemsTemplate.Server
                                                                 si.Status == DirRX.PeriodicActionItemsTemplate.ScheduleItem.Status.Active &&
                                                                 si.StartDate <= oneYearAhead).OrderBy(x => x.StartDate))
       {
-        var mainActionItem = awaitingScheduleItem.RepeatSetting.MainActionItem;
-        
-        if (mainActionItem != null &&
-            mainActionItem.Status == GD.GovernmentSolution.ActionItemExecutionTask.Status.InProcess &&
-            awaitingScheduleItem.StartDate < mainActionItem.Started)
-          continue;
-        
         var scheduleItemBlock = awaitingScheduleItemsBlock.AddChildBlock();
         scheduleItemBlock.Entity = awaitingScheduleItem;
         scheduleItemBlock.AssignIcon(StateBlockIconType.OfEntity, StateBlockIconSize.Small);
